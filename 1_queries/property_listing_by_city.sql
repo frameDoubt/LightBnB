18ea272f33a9b085f2a3c420eaa802bc1e6a7ca7
Brandon Wright <brandon.w.wright4@gmail.com>
2021-02-06 08:32:22 +0000

    add query that gets all users reservations information

--- 1_queries/property_listing_by_city.sql
+++ 1_queries/property_listing_by_city.sql
@@ -1,4 +1,4 @@
-SELECT  properties.*, properties.cost_per_night, AVG(property_reviews.rating) AS average_rating
+SELECT  properties.*, AVG(property_reviews.rating) AS average_rating
 FROM properties
 JOIN property_reviews ON property_reviews.property_id = properties.id
 WHERE properties.city LIKE '%ancouv%'
