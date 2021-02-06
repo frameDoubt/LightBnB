a0d2d23affa38ff660420179a71bd20957a839b3
Brandon Wright <brandon.w.wright4@gmail.com>
2021-02-06 07:54:09 +0000

    add query to find cities with the most visits

--- 1_queries/property_listing_by_city.sql
+++ 1_queries/property_listing_by_city.sql
@@ -2,7 +2,7 @@ SELECT  properties.*, properties.cost_per_night, AVG(property_reviews.rating) AS
 FROM properties
 JOIN property_reviews ON property_reviews.property_id = properties.id
 WHERE properties.city LIKE '%ancouv%'
-GROUP BY properties.property_id
+GROUP BY properties.id
 HAVING avg(property_reviews.rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
